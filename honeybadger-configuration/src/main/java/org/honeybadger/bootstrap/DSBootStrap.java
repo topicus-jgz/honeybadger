1a9a3647f411073c4a2e4c2cc42a6ec481090c84
Thijs Smeenk <Thijs.Smeenk@topicus.nl>
2015-10-20 08:02:59 +0200

    Resteasy removed as dependency in HoneyBadger (By Rick) and added a configuration option when setting up a new service indicating wheter configuration is supplied by default (classpath scanning etc) or through honey

--- honeybadger-configuration/src/main/java/org/honeybadger/bootstrap/DSBootStrap.java
+++ honeybadger-configuration/src/main/java/org/honeybadger/bootstrap/DSBootStrap.java
@@ -1,9 +1,6 @@
 package org.honeybadger.bootstrap;
 
-import org.wildfly.swarm.config.datasources.subsystem.dataSource.DataSource;
-import org.wildfly.swarm.config.datasources.subsystem.jdbcDriver.JdbcDriver;
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.datasources.DatasourcesFraction;
 
 /**
  * //todo move to examples this it's just an example of bootstrapping datasources in an akward way
@@ -16,11 +13,11 @@ public class DSBootStrap implements PreDeploymentBootstrap {
 
 		Container container = configuration.getContainer();
 
-		// and a datasource
+/*		// and a datasource
 		container.subsystem(new DatasourcesFraction().jdbcDriver(
 		 new JdbcDriver("h2").driverDatasourceClassName("org.h2.Driver").xaDatasourceClass("org.h2.jdbcx.JdbcDataSource")
 							 .driverModuleName("com.h2database.h2")).dataSource(
 		 new DataSource("ExampleDS").driverName("h2").connectionUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-									.userName("sa").password("sa")));
+									.userName("sa").password("sa")));*/
 	}
 }
