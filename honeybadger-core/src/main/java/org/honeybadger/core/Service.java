5a74ef42df8baeb980ab37cb0805685295b5a499
Thijs Smeenk <Thijs.Smeenk@topicus.nl>
2015-10-26 13:25:04 +0100

    Fix voor runtime missende dependencies

--- honeybadger-core/src/main/java/org/honeybadger/core/Service.java
+++ honeybadger-core/src/main/java/org/honeybadger/core/Service.java
@@ -1,9 +1,8 @@
 package org.honeybadger.core;
 
-import javax.validation.spi.BootstrapState;
-import org.honeybadger.core.bootstrap.BootstrapBuilder;
 import org.honeybadger.bootstrap.Configuration;
 import org.honeybadger.configuration.ConfigurationBootstrap;
+import org.honeybadger.core.bootstrap.BootstrapBuilder;
 import org.honeybadger.core.jaxrs.Resource;
 
 /**
@@ -13,67 +12,67 @@ import org.honeybadger.core.jaxrs.Resource;
  */
 public abstract class Service {
 
-	private final ConfigurationSetup configurationSetup;
+    private final ConfigurationSetup configurationSetup;
 
-	private Configuration configuration;
+    private Configuration configuration;
 
-	private BootstrapBuilder bootstrapBuilder;
+    private BootstrapBuilder bootstrapBuilder;
 
-	protected Service(ConfigurationSetup configurationSetup) throws Exception {
-		this.configurationSetup = configurationSetup;
-		this.boostrap();
-	}
+    protected Service(ConfigurationSetup configurationSetup) throws Exception {
+        this.configurationSetup = configurationSetup;
+        this.boostrap();
+    }
 
-	/**
-	 * Call this method to org.honeybadger.honeybadger.bootstrap the service. Preferably from a psvm (public static void main)
-	 */
-	public void boostrap() throws Exception {
-		configuration = new Configuration();
+    /**
+     * Call this method to org.honeybadger.honeybadger.bootstrap the service. Preferably from a psvm (public static void main)
+     */
+    public void boostrap() throws Exception {
+        configuration = new Configuration();
 
-		//Configuration has to be bootstrapped first
-		if (ConfigurationSetup.HONEY == configurationSetup) {
-			new ConfigurationBootstrap(System.getProperty("honey.url")).bootstrap(configuration);
-		}
+        //Configuration has to be bootstrapped first
+        if (ConfigurationSetup.HONEY == configurationSetup) {
+            new ConfigurationBootstrap(System.getProperty("honey.url")).bootstrap(configuration);
+        }
 
-		//bootstraps that do not rely on CDI or the container being started
-		bootstrapBuilder().bootstrap(configuration);
+        //bootstraps that do not rely on CDI or the container being started
+        bootstrapBuilder().bootstrap(configuration);
 
-		//Doing the org.honeybadger.honeybadger.bootstrap on the container
-		configuration.getContainer().start();
+        //Doing the org.honeybadger.honeybadger.bootstrap on the container
+        configuration.getContainer().start();
 
-		//User configurationSetup of the container (adding classes etc)
-		setup();
+        //User configurationSetup of the container (adding classes etc)
+        setup();
 
-		//Deploying the container
-		configuration.getJaxrsArchive().addAllDependencies();
-		configuration.deployJaxRS();
-	}
+        //Deploying the container
+        configuration.getJaxrsArchive().addAllDependencies();
+        configuration.deployJaxRS();
+    }
 
-	public BootstrapBuilder bootstrapBuilder() {
-		return new BootstrapBuilder();
-	}
+    public BootstrapBuilder bootstrapBuilder() {
+        return new BootstrapBuilder();
+    }
 
-	public abstract void setup();
+    public abstract void setup();
 
-	protected void registerResource(Class<? extends Resource> resourceClass) {
-		assertBootstrapped();
-		configuration.getJaxrsArchive().addResource(resourceClass);
-	}
+    protected void registerResource(Class<? extends Resource> resourceClass) {
+        assertBootstrapped();
+        configuration.getJaxrsArchive().addResource(resourceClass);
+    }
 
-	protected void addClass(Class<?> classToAdd) {
-		assertBootstrapped();
-		configuration.getJaxrsArchive().addClass(classToAdd);
-	}
+    protected void addClass(Class<?> classToAdd) {
+        assertBootstrapped();
+        configuration.getJaxrsArchive().addClass(classToAdd);
+    }
 
-	protected void addPackage(String pack) {
-		assertBootstrapped();
-		configuration.getJaxrsArchive().addPackages(true, pack);
-	}
+    protected void addPackage(String pack) {
+        assertBootstrapped();
+        configuration.getJaxrsArchive().addPackages(true, pack);
+    }
 
-	private void assertBootstrapped() {
-		if (configuration == null) {
-			throw new IllegalStateException(
-			 "Attempt to use the Configuration but the Configuration was NULL. Was the service bootstrapped before calling this method?");
-		}
-	}
+    private void assertBootstrapped() {
+        if (configuration == null) {
+            throw new IllegalStateException(
+                    "Attempt to use the Configuration but the Configuration was NULL. Was the service bootstrapped before calling this method?");
+        }
+    }
 }
