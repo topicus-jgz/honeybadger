4b2bd02bdd37b7f5edccc67efcdf17437b232494
Thijs Smeenk <Thijs.Smeenk@topicus.nl>
2015-11-03 13:19:52 +0100

    Fixing the version on honeybadger to ALFA5

--- honeybadger-logstash/src/main/java/org/honeybadger/bootstrap/LogStashBootstrapper.java
+++ honeybadger-logstash/src/main/java/org/honeybadger/bootstrap/LogStashBootstrapper.java
@@ -1,7 +1,5 @@
 package org.honeybadger.bootstrap;
 
-import org.honeybadger.bootstrap.Configuration;
-import org.honeybadger.bootstrap.PreDeploymentBootstrap;
 import org.honeybadger.configuration.HoneybadgerProperty;
 import org.wildfly.swarm.logstash.LogstashFraction;
 
@@ -10,19 +8,19 @@ import org.wildfly.swarm.logstash.LogstashFraction;
  */
 public class LogStashBootstrapper implements PreDeploymentBootstrap {
 
-	private static final String EMPTY = "";
-	private String hostname = HoneybadgerProperty.get("common.logstash.hostname");
-	private String level = HoneybadgerProperty.get("common.logstash.level");
-	private String port = HoneybadgerProperty.get("common.logstash.port");
+    private static final String EMPTY = "";
+    private String hostname = HoneybadgerProperty.get("common.logstash.hostname");
+    private String level = HoneybadgerProperty.get("common.logstash.level");
+    private String port = HoneybadgerProperty.get("common.logstash.port");
 
-	public void bootstrap(Configuration configuration) {
+    public void bootstrap(Configuration configuration) {
 
-		if (hostname.equals(EMPTY) || level.equals(EMPTY) || port.equals(EMPTY)) {
-			throw new IllegalStateException(
-			 "Attempt to use honeybader-logstash but not all required properties are set! Check your honey deployment");
-		}
+        if (hostname.equals(EMPTY) || level.equals(EMPTY) || port.equals(EMPTY)) {
+            throw new IllegalStateException(
+                    "Attempt to use honeybader-logstash but not all required properties are set! Check your honey deployment");
+        }
 
-		configuration.getContainer().subsystem(new LogstashFraction().hostname(hostname).level(level).port(port));
-		configuration.ok(this);
-	}
+        configuration.getContainer().fraction(new LogstashFraction().hostname(hostname).level(level).port(port));
+        configuration.ok(this);
+    }
 }
